6839dda40707dbb3b586c25e87dd893e6b36f779
Sakarias Lundgren <sakarias.lundgren@gmail.com>
2024-03-22 14:43:57 +0100

    Added alu and started working on execute_stage.

---
+++ src/alu.sv
@@ -0,0 +1,33 @@
+`timescale 1ps / 1ps
+
+import common::*;
+
+module alu (
+    input wire [2:0] control,
+    input wire [31:0] left_operand,
+    input wire [31:0] right_operand,
+    output logic ZeroFlag,
+    output logic [31:0] result
+);
+
+  always_comb begin
+    case (control)
+      // Shifts (To be added)
+
+      // Arithmetic
+      ALU_ADD: result = left_operand + right_operand;
+      ALU_SUB: result = left_operand - right_operand;
+
+      // Logical
+      ALU_AND: result = left_operand & right_operand;
+      ALU_OR:  result = left_operand | right_operand;
+      //XOR (To be added)
+
+      // Compare (To be added)
+
+      default: result = left_operand + right_operand;
+
+    endcase
+  end
+
+endmodule
---
+++ src/execute_stage.sv
@@ -0,0 +1,43 @@
+`timescale 1ps / 1ps
+
+import common::*;
+
+module execute_stage (
+    input              clk,
+    input              rst,
+    input              ALUSrc,
+    input control_type control_in,
+    input logic [31:0] data1,
+    input logic [31:0] data2,
+
+    output control_type control_out,
+    output        ZeroFlag,
+    output [31:0] alu_data,
+    output [31:0] memory_data
+);
+
+  alu alu (
+      .control(ALUControl),
+      .left_operand(left_operand),
+      .right_operand(right_operand),
+      .ZeroFlag(ZeroFlag),
+      .result(alu_result)
+  );
+
+  logic [31:0] right_operand;
+  logic [31:0] left_operand;
+
+  always_comb begin : operand_selector
+    left_operand  = data1;
+    right_operand = data2;
+    if (control_in.alu_src) begin
+      right_operand = immediate_data;
+    end
+
+
+  end
+
+  assign control_out = control_in;
+  assign memory_data = data2;
+
+endmodule
