b3518cffebac1daf9003cc846775f494a1e3c9ce
Sakarias Lundgren <sakarias.lundgren@gmail.com>
2024-04-23 10:11:45 +0200

    mby now working plz?

--- src/imm_gen.sv
+++ src/imm_gen.sv
@@ -39,11 +39,12 @@ always_comb begin
       imm_gen_output[12] = instruction[31];
       imm_gen_output[11] = instruction[7];
       imm_gen_output[10:5] = instruction[30:25];
-      imm_gen_output[4:0] = instruction[11:8];
+      imm_gen_output[4:1] = instruction[11:8];
+      imm_gen_output[0] = 0;
       if (instruction[31] == 0) begin
-        imm_gen_output[31:12] = 0;
+        imm_gen_output[31:13] = 0;
       end else begin
-        imm_gen_output [31:12] = {20{1'b1}};
+        imm_gen_output [31:13] = {20{1'b1}};
       end
     end
 
