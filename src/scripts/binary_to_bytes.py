1e718f3072bf9e017c101a0448c20f670fdc0217
BenjaminModica <modica.benjamin@gmail.com>
2024-05-21 12:27:09 +0200

    added binary to bytes script

---
+++ src/scripts/binary_to_bytes.py
@@ -0,0 +1,28 @@
+import re
+
+def reverse_bits(bits):
+    # Reverses the order of bits in a string
+    return bits[::-1]
+
+def strip_beginning(lines):
+    regex = r'(^[01]{16,32})$'
+    binary_numbers = []
+    for line in lines:
+        match = re.search(regex, line)
+        if match:
+            binary_numbers.append(match.group(0))
+    return binary_numbers
+
+def process_bits_from_file_to_file(input_filename, output_filename):
+    with open(input_filename, 'r') as input_file, open(output_filename, 'w') as output_file:
+        input_file = strip_beginning(input_file)
+        for line in input_file:
+            line = line.strip()
+            reversed_line = reverse_bits(line)
+            sections = [reversed_line[i:i+8] for i in range(0, len(reversed_line), 8)]
+            for section in sections:
+                section = reverse_bits(section)
+                output_file.write(section + '\n')
+
+# Example usage
+process_bits_from_file_to_file('../../AssembleRisc/output/out_binary.txt', '../instruction_mem.mem')
